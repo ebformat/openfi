d7861989d400ecf3ae607d3cd021a11d7976d896
ebevazaha <ebevazaha@ITEM-S136584.emea.msad.sopra>
2022-10-29 17:08:18 +0200

    creation main

---
+++ src/main/java/com/example/demo/OpenDemoDriver.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class OpenDemoDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConfigurableApplicationContext runp = SpringApplication.run(OpenDemoDriver.class, args);
+		runp.start();
+
+	}
+
+}
